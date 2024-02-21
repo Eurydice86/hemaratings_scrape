4c3753b43ec674a31b7e4ccf6b49d96f3ce8d37d
Eurydice86 <kyriakopoulos.nikos@gmail.com>
2024-02-21 20:41:00 +0200

    cleanup

--- src/event.py
+++ src/event.py
@@ -1,51 +1,23 @@
 from datetime import datetime
-import numpy as np
+
+# import numpy as np
 import requests
 from bs4 import BeautifulSoup
 
 
 def event(link, year):
-    final_categories = []
-    final_participants = []
     event_id = link.split("/")[-2]
     full_url = "https://hemaratings.com" + link
     page = requests.get(full_url)
-    # soup = BeautifulSoup(page.text, features="html.parser")
     soup = BeautifulSoup(page.text, features="lxml")
 
     sp = soup.find("div", id="main")
     event_name = sp.find("h2").text.strip()
 
-    cols = sp.find_all("dt")
-    columns = []
-    for c in cols:
-        columns.append(c.text.strip())
-
     rs = sp.find_all("dd")
     metadata = []
     for r in rs:
         metadata.append(r.text.strip())
-
-    tournaments = sp.find_all("table", class_="table table-striped")[0]
-    cols = tournaments.find_all("th")
-    columns = []
-    for c in cols:
-        columns.append(c.text.strip())
-
-    rows = []
-    rs = tournaments.find_all("td")
-    for r in rs:
-        rows.append(r.text.strip())
-    np_rows = np.array(rows)
-    np_rows = np_rows.reshape(int(len(rows) / len(columns)), len(columns))
-    for row in np_rows:
-        final_categories.append(row[0])
-
-    participants = sp.find_all("table", class_="table table-striped")[1]
-    rs = participants.find_all("a", href=True)
-    for r in rs:
-        if str(r["href"]).split("/")[1] == "fighters":
-            final_participants.append(r["href"].split("/")[-2])
 
     month_name = metadata[0].split(" ")[0].strip()
 
@@ -77,4 +49,4 @@ def event(link, year):
 
 
 if __name__ == "__main__":
-    event("/events/details/1926/", 2023)
+    print(event("/events/details/1926/", 2023))
