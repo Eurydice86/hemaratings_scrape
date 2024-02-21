f480779ef6e7aff86e5b58f755708be224c7f09a
Eurydice86 <kyriakopoulos.nikos@gmail.com>
2024-02-21 20:31:16 +0200

    changed saved data to sqlite

--- src/clubs.py
+++ src/clubs.py
@@ -1,11 +1,31 @@
-import csv
 import requests
+import sqlite3
 from bs4 import BeautifulSoup
 
 from src import club
+from src import sql_helpers
 
 
 def clubs():
+    conn = sqlite3.connect("data/hemaratings.db")
+    cursor = conn.cursor()
+
+    cursor.execute(sql_helpers.drop_table("clubs"))
+    cursor.execute(
+        sql_helpers.create_table(
+            "clubs",
+            {
+                "club_id": "INTEGER PRIMARY KEY",
+                "club_name": "TEXT",
+                "club_short_name": "TEXT",
+                "country": "TEXT",
+                "state": "TEXT",
+                "city": "TEXT",
+                "parent_club_id": "INTEGER",
+            },
+        )
+    )
+
     clubs = requests.get("https://hemaratings.com/clubs/")
     clubs_soup = BeautifulSoup(clubs.text, features="lxml")
 
@@ -13,25 +33,14 @@ def clubs():
     data = table.find_all("a", href=True)
 
     print("Scraping club list")
-    with open("data/clubs.csv", "w", newline="") as clubs_csv:
-        fieldnames = [
-            "club_id",
-            "club_name",
-            "club_short_name",
-            "country",
-            "state",
-            "city",
-            "parent_club_id",
-        ]
 
-        writer = csv.DictWriter(clubs_csv, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for i, d in enumerate(data):
-            print(f"{100 * (i/len(data)):2.2f}% completed.", end="\r")
-            link = d["href"]
-            line = club.club(link)
-            writer.writerow(line)
+    for i, d in enumerate(data):
+        print(f"{100 * (i/len(data)):2.2f}% completed.", end="\r")
+        link = d["href"]
+        line = club.club(link)
+        cursor.execute(sql_helpers.insert("clubs", line))
+    conn.commit()
+    conn.close()
 
 
 if __name__ == "__main__":
